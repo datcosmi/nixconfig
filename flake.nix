873f80b15d18f04083a38c33563680c35106540f
Iván García <garcia.cli@pm.me>
2026-05-11 15:25:31 -0600

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,62 @@
+{
+  description = "My Nix Flake";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+
+    niri = {
+      # url = "github:niri-wm/niri";
+      url = "github:sodiboo/niri-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    awww = {
+      url = "git+https://codeberg.org/LGFae/awww";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    catppuccin = {
+      url = "github:catppuccin/nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    zen-browser = {
+      url = "github:youwen5/zen-browser-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    home-manager,
+    ...
+  } @ inputs: let
+    lib = nixpkgs.lib;
+    myLib = import ./lib {inherit lib inputs;};
+  in {
+    nixosConfigurations = {
+      suavicrema = myLib.mkHost {
+        hostname = "suavicrema";
+        system = "x86_64-linux";
+        users = ["ivan"];
+      };
+
+      mandarina = myLib.mkHost {
+        hostname = "mandarina";
+        system = "x86_64-linux";
+        users = ["ivan"];
+      };
+    };
+  };
+}
