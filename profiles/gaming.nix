1c75c53b63469d4c421f0f99eb92707dae646ee8
Iván García <garcia.cli@pm.me>
2026-05-14 16:13:36 -0600

    add personal & gaming profiles

---
+++ profiles/gaming.nix
@@ -0,0 +1,3 @@
+{
+  my.features.gaming.enable = true;
+}
---
+++ profiles/personal.nix
@@ -0,0 +1,10 @@
+{
+  my.features = {
+    desktop.enable = true;
+
+    system = {
+      services.enable = true;
+      networking.enable = true;
+    };
+  };
+}
