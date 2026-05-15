b89f02ac647b92c2a5125b7ebfa0514617044f8d
Iván García <garcia.cli@pm.me>
2026-05-14 22:08:28 -0600

    fix explicit & implicit mode mismatch on graphics nixos module

--- modules/nixos/graphics/default.nix
+++ modules/nixos/graphics/default.nix
@@ -9,22 +9,25 @@
 in {
   imports = [./nvidia.nix];
 
-  services.xserver.enable = true;
-  services.xserver.xkb = {
-    layout = kbd.layout;
-    variant = kbd.variant;
-  };
+  config = lib.mkMerge [
+    {
+      services.xserver.enable = true;
+      services.xserver.xkb = {
+        layout = kbd.layout;
+        variant = kbd.variant;
+      };
+      hardware.graphics = {
+        enable = true;
+        enable32Bit = true;
+      };
+    }
 
-  hardware.graphics = {
-    enable = true;
-    enable32Bit = true;
-  };
-
-  config = lib.mkIf cfg.enable {
-    xdg.portal = {
-      enable = true;
-      extraPortals = [pkgs.xdg-desktop-portal-gtk];
-      config.common.default = "gtk";
-    };
-  };
+    (lib.mkIf cfg.enable {
+      xdg.portal = {
+        enable = true;
+        extraPortals = [pkgs.xdg-desktop-portal-gtk];
+        config.common.default = "gtk";
+      };
+    })
+  ];
 }
