f35760c405c3a74c81de8ff5aafb9556fb5ebc65
Iván García <garcia.cli@pm.me>
2026-05-15 13:49:01 -0600

    add default module to terminals

--- modules/home/terminal/default.nix
+++ modules/home/terminal/default.nix
@@ -1,6 +1,19 @@
 {
+  config,
+  lib,
+  ...
+}: let
+  cfg = config.my.features.terminal;
+in {
   imports = [
     ./kitty.nix
     ./ghostty.nix
   ];
+
+  options.my.features.terminal.enable = lib.mkEnableOption "Enable all terminal emulators";
+
+  config = lib.mkIf cfg.enable {
+    my.features.terminal.kitty.enable = lib.mkDefault true;
+    my.features.terminal.ghostty.enable = lib.mkDefault true;
+  };
 }
