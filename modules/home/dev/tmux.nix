4db55692ef911d6a0241a74f1fecccc8d9884ae6
Iván García <garcia.cli@pm.me>
2026-05-14 22:22:21 -0600

    add catppuccin theme to tmux home module

--- modules/home/dev/tmux.nix
+++ modules/home/dev/tmux.nix
@@ -5,34 +5,55 @@
   ...
 }: let
   cfg = config.my.features.dev.tmux;
+  theme = config.my.theme;
 in {
   options.my.features.dev.tmux.enable = lib.mkEnableOption "Enable tmux";
 
-  config = mkIf cfg.enable {
-    programs.tmux = {
-      enable = true;
-      shortcut = "s";
-      mouse = true;
-      baseIndex = 1;
-      terminal = "tmux-256color";
+  config = lib.mkMerge [
+    (lib.mkIf (cfg.enable) {
+      programs.tmux = {
+        enable = true;
+        shortcut = "s";
+        mouse = true;
+        baseIndex = 1;
+        terminal = "tmux-256color";
 
-      plugins = with pkgs.tmuxPlugins; [
-        sensible
-        yank
-        vim-tmux-navigator
-        resurrect
-        continuum
-      ];
+        plugins = with pkgs.tmuxPlugins; [
+          sensible
+          yank
+          vim-tmux-navigator
+          resurrect
+          continuum
+        ];
 
-      extraConfig = ''
-        bind-key h select-pane -L
-        bind-key j select-pane -D
-        bind-key k select-pane -U
-        bind-key l select-pane -R
+        extraConfig = ''
+          bind-key h select-pane -L
+          bind-key j select-pane -D
+          bind-key k select-pane -U
+          bind-key l select-pane -R
 
-        set -g status-style bg=default
-        set-option -g status-position top
-      '';
-    };
-  };
+          set -g status-style bg=default
+          set-option -g status-position top
+        '';
+      };
+    })
+
+    (lib.mkIf (cfg.enable && theme.palette == "catppuccin-mocha") {
+      catppuccin.tmux = {
+        enable = true;
+        flavor = "mocha";
+        extraConfig = ''
+          set -g @catppuccin_window_status_style "rounded"
+          set -g status-right-length 100
+          set -g status-left-length 100
+          set -g status-left ""
+          set -g status-right "#{E:@catppuccin_status_application}"
+          set -ag status-right "#{E:@catppuccin_status_session}"
+          set -g @catppuccin_window_default_text " #W"
+          set -g @catppuccin_window_current_text " #W"
+          set -g @catppuccin_window_text " #W"
+        '';
+      };
+    })
+  ];
 }
