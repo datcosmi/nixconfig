a25877bb7c1d021658e08d52a51c9ca99df82b53
Iván García <garcia.cli@pm.me>
2026-05-15 21:25:59 -0600

    delete option to enable all desktop things
    it didn't make any sense actually

--- modules/home/desktop/default.nix
+++ modules/home/desktop/default.nix
@@ -1,22 +1,9 @@
 {
-  config,
-  lib,
-  ...
-}: let
-  cfg = config.my.features.desktop;
-in {
-  options.my.features.desktop.enable = lib.mkEnableOption "Enable all desktop app, utilities, etc.";
-
-  config = lib.mkIf cfg.enable {
-    my.features.desktop = {
-      apps.enable = lib.mkDefault true;
-    };
-  };
-
   imports = [
     ./apps
     ./wm
     ./launchers
     ./clipboard
+    ./wallpaper
   ];
 }
