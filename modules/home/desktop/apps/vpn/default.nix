045021133737099d69e1aaa936fb9ffd3036a01a
Iván García <garcia.cli@pm.me>
2026-05-15 20:34:36 -0600

    add vpn home modules

---
+++ modules/home/desktop/apps/vpn/default.nix
@@ -0,0 +1,19 @@
+{
+  config,
+  lib,
+  ...
+}: let
+  cfg = config.my.features.desktop.apps.vpn;
+in {
+  options.my.features.desktop.apps.vpn.enable = lib.mkEnableOption "Enable all vpn apps";
+
+  config = lib.mkIf cfg.enable {
+    my.features.desktop.apps.vpn = {
+      proton-vpn.enable = lib.mkDefault true;
+    };
+  };
+
+  imports = [
+    ./proton-vpn.nix
+  ];
+}
