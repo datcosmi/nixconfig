7f0c37761518e1cecd4f290d90ef3b351cdcbd51
Iván García <garcia.cli@pm.me>
2026-05-15 20:15:18 -0600

    add option to enable all file manager apps
    even tho it's just nautilus rn...

--- modules/home/desktop/apps/file-managers/default.nix
+++ modules/home/desktop/apps/file-managers/default.nix
@@ -1,4 +1,18 @@
 {
+  config,
+  lib,
+  ...
+}: let
+  cfg = config.my.features.desktop.apps.file-managers;
+in {
+  options.my.features.desktop.apps.file-managers.enable = lib.mkEnableOption "Enable all File Manager apps";
+
+  config = lib.mkIf cfg.enable {
+    my.features.desktop.apps.file-managers = {
+      nautilus.enable = mkDefault true;
+    };
+  };
+
   imports = [
     ./nautilus.nix
   ];
