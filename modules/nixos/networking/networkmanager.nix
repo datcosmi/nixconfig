16c8be7c12507c237247042b61aee554c63d7a68
Iván García <garcia.cli@pm.me>
2026-05-15 21:06:40 -0600

    enable iwd & use it as nw wireless backend if host has wifi

--- modules/nixos/networking/networkmanager.nix
+++ modules/nixos/networking/networkmanager.nix
@@ -5,18 +5,26 @@
   ...
 }: let
   cfg = config.my.features.system.networking.networkManager;
+  wifi = config.my.hardware.hasWifi;
 in {
   options.my.features.system.networking.networkManager.enable = lib.mkEnableOption "NetworkManager service";
 
-  config = lib.mkIf cfg.enable {
-    networking.networkmanager = {
-      enable = true;
-    };
+  config = lib.mkMerge [
+    (lib.mkIf cfg.enable {
+      networking.networkmanager = {
+        enable = true;
+      };
 
-    systemd.services.NetworkManager-wait-online.enable = false;
+      systemd.services.NetworkManager-wait-online.enable = false;
 
-    environment.systemPackages = with pkgs; [
-      networkmanagerapplet
-    ];
-  };
+      environment.systemPackages = with pkgs; [
+        networkmanagerapplet
+      ];
+    })
+
+    (lib.mkIf wifi {
+      networking.wireless.iwd.enable = true;
+      networking.networkmanager.wifi.backend = "iwd";
+    })
+  ];
 }
