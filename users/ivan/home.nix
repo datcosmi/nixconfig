42a9d9b5e4bf5c5a19868510933341ea99ea025e
Iván García <garcia.cli@pm.me>
2026-05-16 15:48:10 -0600

    add user specific features & default apps

--- users/ivan/home.nix
+++ users/ivan/home.nix
@@ -19,10 +19,35 @@
     };
   };
 
-  my.features.theme = {
-    palette = "catppuccin-mocha";
-    gtk = "adwaita";
-    cursor = "bibata-ice";
+  my.features = {
+    theme = {
+      palette = "catppuccin-mocha";
+      gtk = "adwaita";
+      cursor = "bibata-ice";
+    };
+
+    desktop.apps.enable = true;
+    dev.enable = true;
+    shell.enable = true;
+    terminal.kitty.enable = true;
+  };
+
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "image/jpeg" = "org.gnome.Loupe.desktop";
+      "image/jpg" = "org.gnome.Loupe.desktop";
+      "image/png" = "org.gnome.Loupe.desktop";
+      "image/*" = "org.gnome.Loupe.desktop";
+
+      "text/html" = "zen.desktop";
+      "x-scheme-handler/http" = "zen.desktop";
+      "x-scheme-handler/https" = "zen.desktop";
+      "x-scheme-handler/about" = "zen.desktop";
+      "x-scheme-handler/unknown" = "zen.desktop";
+
+      "application/pdf" = "org.pwmt.zathura.desktop";
+    };
   };
 
   home.packages = with pkgs; [
