2a9743c7193abf976902836dd6549b4f4466eda7
Iván García <garcia.cli@pm.me>
2026-05-17 16:54:21 -0600

    only install rofimoji if emoji is enabled

--- modules/home/desktop/launchers/rofi.nix
+++ modules/home/desktop/launchers/rofi.nix
@@ -6,14 +6,18 @@
 }: let
   wallpaper = config.my.features.theme.rofi-bg;
   cfg = config.my.features.desktop.launchers.rofi;
+  emoji = config.my.features.desktop.fonts.emoji;
 in {
   options.my.features.desktop.launchers.rofi.enable = lib.mkEnableOption "Use rofi launcher";
   config = lib.mkIf cfg.enable {
+    home.packages = lib.mkIf emoji.enable (with pkgs; [
+      rofimoji
+    ]);
+
     programs.rofi = {
       enable = true;
       plugins = [
         pkgs.rofi-calc
-        pkgs.rofimoji
       ];
 
       font = "JetBrains Mono Nerd Font 10";
