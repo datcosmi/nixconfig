d6b254c4333bbbd36689ef4f99766a7136b91574
Iván García <garcia.cli@pm.me>
2026-05-17 19:11:10 -0600

    add libreoffice module

---
+++ modules/home/desktop/apps/productivity/libreoffice.nix
@@ -0,0 +1,16 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
+  cfg = config.my.features.desktop.apps.productivity.libreoffice;
+in {
+  options.my.features.desktop.apps.productivity.libreoffice.enable = lib.mkEnableOption "Enable LibreOffice suite";
+
+  config = lib.mkIf cfg.enable {
+    home.packages = with pkgs; [
+      libreoffice-fresh
+    ];
+  };
+}
