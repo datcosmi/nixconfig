d2215d7cc1b9669a0a485cf741b7145209af14c8
Iván García <garcia.cli@pm.me>
2026-05-18 13:28:42 -0600

    desktop profile forces polkit agent module to enable

--- profiles/desktop.nix
+++ profiles/desktop.nix
@@ -3,7 +3,11 @@
     desktop.enable = lib.mkDefault true;
 
     system = {
-      services.enable = lib.mkDefault true;
+      services = {
+        enable = lib.mkDefault true;
+        security.polkitAgent.enable = lib.mkForce true;
+      };
+
       networking.enable = lib.mkDefault true;
     };
   };
