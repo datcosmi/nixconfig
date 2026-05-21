8481a59ebd6ac2d61a50554ae0980c7942dc3b28
Iván García <garcia.cli@pm.me>
2026-05-21 12:32:04 -0600

    add activation script to fix some user directories being owned by root

--- lib/default.nix
+++ lib/default.nix
@@ -28,6 +28,21 @@ in rec {
           {my.users = users;}
 
           {
+            system.activationScripts.createNixUserProfiles = lib.stringAfter ["users"] ''
+              for user in ${lib.concatStringsSep " " userNames}; do
+                home=$(getent passwd $user | cut -d: -f6)
+                mkdir -p /nix/var/nix/profiles/per-user/$user
+                chown $user:users /nix/var/nix/profiles/per-user/$user
+                mkdir -p /nix/var/nix/gcroots/per-user/$user
+                chown $user:users /nix/var/nix/gcroots/per-user/$user
+                mkdir -p $home/.local/state/nix/profiles
+                mkdir -p $home/.local/share
+                mkdir -p $home/.config
+                mkdir -p $home/.cache
+                chown -R $user:users $home
+              done
+            '';
+
             home-manager = {
               useGlobalPkgs = true;
               useUserPackages = true;
