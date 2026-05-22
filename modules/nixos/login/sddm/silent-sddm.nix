2ab7a7e027a476d656512b637fface45b7efc3a8
Iván García <garcia.cli@pm.me>
2026-05-21 23:53:23 -0600

    add avatar support per user to silentSDDM

--- modules/nixos/login/sddm/silent-sddm.nix
+++ modules/nixos/login/sddm/silent-sddm.nix
@@ -14,5 +14,17 @@ in {
     programs.silentSDDM = {
       enable = true;
     };
+
+    systemd.tmpfiles.rules =
+      lib.concatMap (
+        user: let
+          avatarPath = ../../../users/${user}/avatar.png;
+        in
+          lib.optionals (builtins.pathExists avatarPath) [
+            "f+ /var/lib/AccountsService/users/${user}  0600 root root -  [User]\\nIcon=/var/lib/AccountsService/icons/${user}\\n"
+            "L+ /var/lib/AccountsService/icons/${user}  -    -    -    -  ${avatarPath}"
+          ]
+      )
+      config.my.users;
   };
 }
