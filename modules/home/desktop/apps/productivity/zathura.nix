93d91de096d252e95727ae23340900e38f2f91dc
Iván García <garcia.cli@pm.me>
2026-05-21 20:53:02 -0600

    add catppuccin theme to zathura

--- modules/home/desktop/apps/productivity/zathura.nix
+++ modules/home/desktop/apps/productivity/zathura.nix
@@ -5,10 +5,16 @@
   ...
 }: let
   cfg = config.my.features.desktop.apps.productivity.zathura;
+  theme = config.my.features.theme;
 in {
   options.my.features.desktop.apps.productivity.zathura.enable = lib.mkEnableOption "Enable Zathura app";
 
   config = lib.mkIf cfg.enable {
     programs.zathura.enable = true;
+
+    catppuccin.zathura = lib.mkIf (theme == "catppuccin-mocha") {
+      enable = true;
+      flavor = "mocha";
+    };
   };
 }
