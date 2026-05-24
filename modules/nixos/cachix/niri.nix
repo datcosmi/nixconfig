68ed926a4e81b7f772e77beee317e36040eab217
Iván García <garcia.cli@pm.me>
2026-05-24 16:18:27 -0600

    only charge niri cache if niri is enabled

--- modules/nixos/cachix/niri.nix
+++ modules/nixos/cachix/niri.nix
@@ -1,13 +1,20 @@
-
 {
-  nix = {
-    settings = {
-      substituters = [
-        "https://niri.cachix.org"
-      ];
-      trusted-public-keys = [
-        "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964="
-      ];
+  config,
+  lib,
+  ...
+}: let
+  cfg = config.my.features.desktop.niri;
+in {
+  config = lib.mkIf.cfg.enable {
+    nix = {
+      settings = {
+        substituters = [
+          "https://niri.cachix.org"
+        ];
+        trusted-public-keys = [
+          "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964="
+        ];
+      };
     };
   };
 }
