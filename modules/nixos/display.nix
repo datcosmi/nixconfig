1b4fd6e0017066644aca5d578b9828b6d8681f77
Iván García <garcia.cli@pm.me>
2026-05-27 02:11:35 -0600

    try to expose ddc monitors as regular backlight devices
    spoiler: didn't work

--- modules/nixos/display.nix
+++ modules/nixos/display.nix
@@ -17,14 +17,20 @@ in {
     (lib.mkIf cfg.ddc {
       hardware.i2c.enable = true;
 
-      boot.kernelModules = lib.mkAfter ["i2c-dev"];
+      boot.extraModulePackages = with config.boot.kernelPackages; [ddcci-driver];
+      boot.kernelModules = lib.mkAfter ["i2c-dev" "ddcci" "ddcci-backlight"];
 
       environment.systemPackages = with pkgs; [
         ddcutil
         i2c-tools
       ];
 
-      services.udev.extraRules = ''
+      services.udev.extraRules = let
+        bash = "${pkgs.bash}/bin/bash";
+        ddcciDev = "AUX USBC4/DDI TC4/PHY TC4";
+        ddcciNode = "/sys/bus/i2c/devices/i2c-17/new_device";
+      in ''
+        SUBSYSTEM=="i2c", ACTION=="add", ATTR{name}=="${ddcciDev}", RUN+="${bash} -c 'sleep 30; printf ddcci\ 0x37 > ${ddcciNode}'"
         KERNEL=="i2c-[0-9]*", GROUP="i2c", MODE="0660"
       '';
     })
