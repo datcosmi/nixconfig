7d292e26ad44dc19b7f9b416a3e2c467fff688e0
Iván García <garcia.cli@pm.me>
2026-05-27 02:10:50 -0600

    new audio module

--- modules/nixos/services/audio.nix
+++ modules/nixos/services/audio.nix
@@ -1,11 +1,26 @@
 {
   lib,
   config,
+  pkgs,
   ...
 }: let
   cfg = config.my.features.system.services.audio;
 in {
-  options.my.features.system.services.audio.enable = lib.mkEnableOption "Audio service";
+  options.my.features.system.services.audio = {
+    enable = lib.mkEnableOption "Audio service";
+
+    defaultVolume = lib.mkOption {
+      type = lib.types.str;
+      default = "70%";
+      description = "Default audio volume applied on login.";
+    };
+
+    unmuteOnLogin = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = "Whether to unmute audio on login.";
+    };
+  };
 
   config = lib.mkIf cfg.enable {
     services.pulseaudio.enable = false;
@@ -16,6 +31,32 @@ in {
       alsa.enable = true;
       alsa.support32Bit = true;
       pulse.enable = true;
+      wireplumber.enable = true;
+    };
+
+    systemd.user.services.default-volume = {
+      description = "Set default audio volume";
+
+      after = [
+        "pipewire.service"
+        "wireplumber.service"
+      ];
+
+      wantedBy = ["default.target"];
+
+      serviceConfig = {
+        Type = "oneshot";
+
+        ExecStart = let
+          muteCmd =
+            if cfg.unmuteOnLogin
+            then "${pkgs.wireplumber}/bin/wpctl set-mute @DEFAULT_AUDIO_SINK@ 0"
+            else "";
+        in ''
+          ${pkgs.wireplumber}/bin/wpctl set-volume @DEFAULT_AUDIO_SINK@ ${cfg.defaultVolume}
+          ${muteCmd}
+        '';
+      };
     };
   };
 }
