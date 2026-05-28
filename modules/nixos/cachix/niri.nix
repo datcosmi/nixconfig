8712b2a9215f0d4ac1cab41745c54c050f857ada
Iván García <garcia.cli@pm.me>
2026-05-28 13:20:21 -0600

    remove unnecessary conditionals on cachix modules

--- modules/nixos/cachix/niri.nix
+++ modules/nixos/cachix/niri.nix
@@ -1,20 +1,12 @@
-{
-  config,
-  lib,
-  ...
-}: let
-  cfg = config.my.features.desktop.niri;
-in {
-  config = lib.mkIf cfg.enable {
-    nix = {
-      settings = {
-        substituters = [
-          "https://niri.cachix.org"
-        ];
-        trusted-public-keys = [
-          "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964="
-        ];
-      };
+{...}: {
+  nix = {
+    settings = {
+      substituters = [
+        "https://niri.cachix.org"
+      ];
+      trusted-public-keys = [
+        "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964="
+      ];
     };
   };
 }
--- modules/nixos/cachix/noctalia.nix
+++ modules/nixos/cachix/noctalia.nix
@@ -1,14 +1,6 @@
-{
-  config,
-  lib,
-  ...
-}: let
-  cfg = config.my.features.desktop.noctalia;
-in {
-  config = lib.mkIf cfg.enable {
-    nix.settings = {
-      extra-substituters = ["https://noctalia.cachix.org"];
-      extra-trusted-public-keys = ["noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="];
-    };
+{...}: {
+  nix.settings = {
+    extra-substituters = ["https://noctalia.cachix.org"];
+    extra-trusted-public-keys = ["noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="];
   };
 }
