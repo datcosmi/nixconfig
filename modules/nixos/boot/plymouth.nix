ffac6156b9979ec4ea23fdf27752b0cf43b3cb35
Iván García <garcia.cli@pm.me>
2026-05-29 17:36:24 -0600

    add plymouth theme & silent boot

--- modules/nixos/boot/plymouth.nix
+++ modules/nixos/boot/plymouth.nix
@@ -9,6 +9,27 @@ in {
   options.my.features.boot.plymouth.enable = lib.mkEnableOption "Enable Plymouth on boot";
 
   config = lib.mkIf cfg.enable {
-    boot.plymouth.enable = true;
+    boot = {
+      plymouth = {
+        enable = true;
+        theme = "cuts_alt";
+        themePackages = with pkgs; [
+          (adi1090x-plymouth-themes.override {
+            selected_themes = ["cuts_alt"];
+          })
+        ];
+      };
+
+      initrd.systemd.enable = true;
+
+      # Enable "Silent boot"
+      consoleLogLevel = 3;
+      initrd.verbose = false;
+      kernelParams = [
+        "quiet"
+        "udev.log_level=3"
+        "systemd.show_status=auto"
+      ];
+    };
   };
 }
