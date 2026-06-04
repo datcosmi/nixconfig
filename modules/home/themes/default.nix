27e41bb7b3d50e61fb052f03a4a131bb4e256866
Iván García <garcia.cli@pm.me>
2026-06-03 21:00:33 -0600

    symlink wallpapers into user directories

--- modules/home/themes/default.nix
+++ modules/home/themes/default.nix
@@ -2,41 +2,50 @@
   lib,
   config,
   ...
-}:
-with lib; {
-  options.my.features.theme = {
-    palette = mkOption {
-      type = types.enum ["catppuccin-mocha"];
-      default = "catppuccin-mocha";
+}: let
+  cfg = config.my.features.desktop.userDirectories;
+in
+  with lib; {
+    options.my.features.theme = {
+      palette = mkOption {
+        type = types.enum ["catppuccin-mocha"];
+        default = "catppuccin-mocha";
+      };
+
+      gtk = mkOption {
+        type = types.enum ["adwaita"];
+        default = "adwaita";
+      };
+
+      cursor = mkOption {
+        type = types.enum ["bibata-classic" "bibata-ice" "nordzy"];
+        default = "bibata-ice";
+      };
+
+      wallpaper = mkOption {
+        type = types.path;
+      };
+
+      rofi-bg = mkOption {
+        type = types.path;
+      };
+
+      colors = mkOption {
+        type = types.attrsOf types.str;
+        default = {};
+      };
     };
 
-    gtk = mkOption {
-      type = types.enum ["adwaita"];
-      default = "adwaita";
+    config = mkIf cfg.enable {
+      home.file."${config.xdg.userDirs.pictures}/Wallpapers" = {
+        source = ./wallpapers;
+        recursive = true;
+      };
     };
 
-    cursor = mkOption {
-      type = types.enum ["bibata-classic" "bibata-ice" "nordzy"];
-      default = "bibata-ice";
-    };
-
-    wallpaper = mkOption {
-      type = types.path;
-    };
-
-    rofi-bg = mkOption {
-      type = types.path;
-    };
-
-    colors = mkOption {
-      type = types.attrsOf types.str;
-      default = {};
-    };
-  };
-
-  imports = [
-    ./palettes
-    ./gtk
-    ./cursor
-  ];
-}
+    imports = [
+      ./palettes
+      ./gtk
+      ./cursor
+    ];
+  }
