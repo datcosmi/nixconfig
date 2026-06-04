efbb7230f1ac0f33635cf702963841961bb7ab46
Iván García <garcia.cli@pm.me>
2026-06-03 21:02:27 -0600

    add: swayidle to handle lock when noctalia is enabled

--- modules/home/desktop/shell/noctalia/default.nix
+++ modules/home/desktop/shell/noctalia/default.nix
@@ -7,6 +7,8 @@
 }: let
   cfg = config.my.features.desktop.shell.noctalia;
   wallpaper = config.my.features.theme.wallpaper;
+  noctaliaShell = config.programs.noctalia-shell.package;
+  lockCmd = "${noctaliaShell}/bin/noctalia-shell ipc call lockScreen lock";
 in {
   imports = [
     inputs.noctalia.homeModules.default
@@ -22,6 +24,15 @@ in {
     home.file.".cache/noctalia/wallpapers.json" = {
       text = builtins.toJSON {
         defaultWallpaper = "${wallpaper}";
+      };
+    };
+
+    services.swayidle = {
+      enable = true;
+      systemdTargets = ["graphical-session.target"];
+      events = {
+        lock = lockCmd;
+        before-sleep = lockCmd;
       };
     };
   };
