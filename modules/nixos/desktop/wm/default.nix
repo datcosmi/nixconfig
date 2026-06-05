723608f8ab48e92c3623cd729a3e06f23354d181
Iván García <garcia.cli@pm.me>
2026-06-05 14:40:15 -0600

    add: wl-mirror package

--- modules/nixos/desktop/wm/default.nix
+++ modules/nixos/desktop/wm/default.nix
@@ -1,7 +1,20 @@
-{lib, ...}: {
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}: let
+  cfg = config.my.features.wayland;
+in {
   imports = [
     ./niri
   ];
 
   options.my.features.wayland.enable = lib.mkEnableOption "Wayland session and related environment";
+
+  config = lib.mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [
+      wl-mirror
+    ];
+  };
 }
