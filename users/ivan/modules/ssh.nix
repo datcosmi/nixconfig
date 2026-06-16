48e97db5f57b00942ef7ce65befb292b9918e4c1
Iván García <garcia.cli@pm.me>
2026-06-15 23:23:00 -0600

    add host ssh keys to agent based on host

--- users/ivan/modules/ssh.nix
+++ users/ivan/modules/ssh.nix
@@ -8,13 +8,24 @@
   cfg = config.my.features.ssh;
 in {
   config = lib.mkIf cfg.enable {
-    programs.ssh.settings = lib.mkIf (hostname != "mandarina") {
-      "mandarina" = {
-        HostName = "mandarina.local";
-        User = "ivan";
-        IdentityFile = "~/.ssh/mandarina";
-        AddKeysToAgent = "yes";
-      };
-    };
+    programs.ssh.settings = lib.mkMerge [
+      (lib.mkIf (hostname != "mandarina") {
+        "mandarina" = {
+          HostName = "mandarina.local";
+          User = "ivan";
+          IdentityFile = "~/.ssh/mandarina";
+          AddKeysToAgent = "yes";
+        };
+      })
+
+      (lib.mkIf (hostname != "suavicrema") {
+        "suavicrema" = {
+          HostName = "suavicrema.local";
+          User = "ivan";
+          IdentityFile = "~/.ssh/suavicrema";
+          AddKeysToAgent = "yes";
+        };
+      })
+    ];
   };
 }
