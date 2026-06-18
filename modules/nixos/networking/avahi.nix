7b0cdf5a9feb6f09dd2a7a13697c1cef8ec03d1a
Iván García <garcia.cli@pm.me>
2026-06-17 21:35:51 -0600

    avahi: add publish options & enable ipv4 ipv6

--- modules/nixos/networking/avahi.nix
+++ modules/nixos/networking/avahi.nix
@@ -11,12 +11,16 @@ in {
   config = lib.mkIf cfg.enable {
     services.avahi = {
       enable = true;
+      ipv4 = true;
+      ipv6 = true;
       nssmdns4 = true;
       nssmdns6 = true;
       openFirewall = true;
 
       publish = {
         enable = true;
+        domain = true;
+        addresses = true;
         userServices = true;
       };
     };
