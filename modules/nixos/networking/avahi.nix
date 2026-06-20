e187c55df64f890f80d72e7f5b56ad84ad6cf6bf
Iván García <garcia.cli@pm.me>
2026-06-19 20:58:26 -0600

    avahi: get rid of everything to see if it works because I'm tired and don't know what else to do

--- modules/nixos/networking/avahi.nix
+++ modules/nixos/networking/avahi.nix
@@ -11,39 +11,7 @@ in {
   config = lib.mkIf cfg.enable {
     services.avahi = {
       enable = true;
-      ipv4 = true;
-      ipv6 = true;
-      nssmdns4 = true;
-      nssmdns6 = true;
       openFirewall = true;
-      publish = {
-        enable = true;
-        domain = true;
-        addresses = true;
-        userServices = true;
-      };
     };
-
-    systemd.services.avahi-daemon = {
-      partOf = lib.mkForce [];
-    };
-
-    systemd.services.avahi-daemon-resume = {
-      description = "Restart Avahi mDNS/DNS-SD after system resume";
-      wantedBy = ["post-resume.target"];
-      after = [
-        "post-resume.target"
-        "network-online.target"
-      ];
-      wants = ["network-online.target"];
-      serviceConfig = {
-        Type = "oneshot";
-        ExecStartPre = "${pkgs.coreutils}/bin/sleep 3";
-        ExecStart = "${pkgs.systemd}/bin/systemctl restart avahi-daemon.service";
-        RemainAfterExit = false;
-      };
-    };
-
-    services.avahi.allowInterfaces = ["enp5s0" "wlan0"];
   };
 }
