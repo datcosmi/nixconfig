488b0abb87665a512653eaf2c74b638eb86b3ca5
Iván García <cli.garcia@pm.me>
2026-07-06 16:10:00 -0600

    switch cantarell-fonts from stable to unstable
    finally they are no longer broken

--- modules/home/themes/gtk/adwaita.nix
+++ modules/home/themes/gtk/adwaita.nix
@@ -85,7 +85,7 @@ in
       home.sessionVariables.GTK_THEME = gtkTheme;
 
       home.packages = with pkgs; [
-        stable.cantarell-fonts
+        cantarell-fonts
         source-code-pro
       ];
     };
