ba469df73736cf3617f08975f4a23c2773684cc7
Iván García <cli.garcia@pm.me>
2026-07-29 13:23:20 -0600

    defaultApps: new config

--- users/ivan/home.nix
+++ users/ivan/home.nix
@@ -63,19 +63,18 @@
 
   xdg.mimeApps = {
     enable = true;
-    defaultApplications = {
-      "image/jpeg" = "org.gnome.Loupe.desktop";
-      "image/jpg" = "org.gnome.Loupe.desktop";
-      "image/png" = "org.gnome.Loupe.desktop";
-      "image/*" = "org.gnome.Loupe.desktop";
+    defaultApplicationPackages = with pkgs; [
+      loupe
+      zathura
+      celluloid
+    ];
 
+    defaultApplications = {
       "text/html" = "zen.desktop";
       "x-scheme-handler/http" = "zen.desktop";
       "x-scheme-handler/https" = "zen.desktop";
       "x-scheme-handler/about" = "zen.desktop";
       "x-scheme-handler/unknown" = "zen.desktop";
-
-      "application/pdf" = "org.pwmt.zathura.desktop";
     };
   };
 
